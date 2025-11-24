80a60c714d08f826ef5856906b5966e8bd0c9c81
Anasmtaweh <anasanasmtaweh@email.com>
2025-11-24 20:55:18 +0200

    Model data

---
+++ lib/car_care_data.dart
@@ -0,0 +1,19 @@
+class CarCareData {
+  final String brand;
+  final String oil;
+  final String battery;
+  final String tire;
+  final String brakes;
+  final String airFilter;
+  final String spark;
+
+  CarCareData(
+    this.brand,
+    this.oil,
+    this.battery,
+    this.tire,
+    this.brakes,
+    this.airFilter,
+    this.spark,
+  );
+}
